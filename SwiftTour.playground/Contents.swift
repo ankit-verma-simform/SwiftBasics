eaf961f12b14d70d373a47b4514067c68a6693b3
ankit-verma-simform <ankit.verma@simformsolutions.com>
2023-02-21 17:21:20 +0530

    conditionals in swift

--- SwiftTour.playground/Contents.swift
+++ SwiftTour.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var marks = 110
+
+//using if-else-if ladder
+if marks > 100 || marks < 0 {
+    print("Invalid Marks")
+}
+else if marks > 90 {
+    print("A Grade")
+}
+else if marks > 80 {
+    print("B Grade")
+}
+else if marks > 70 {
+    print("C Grade")
+}
+else if marks > 60 {
+    print("D Grade")
+}
+else {
+    print("Failed")
+}
+
+
+var country = "US"
+switch country {
+case "US":
+    print("United States of America")
+case "IND":
+    print("India")
+default:
+    print("Unknown country")
+}
