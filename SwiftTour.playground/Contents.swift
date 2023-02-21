f47305edfd6f96b68d15db7c27ce7b7fdb07ca7c
ankit-verma-simform <ankit.verma@simformsolutions.com>
2023-02-21 17:36:36 +0530

    loops

--- SwiftTour.playground/Contents.swift
+++ SwiftTour.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//using ranged forloop
+for i in 1...10 {
+    print(i)
+}
+
+//using repeat while
+var j = 0
+repeat {
+    print(j)
+    //++j //we don't have pre or
+    //j++ //post increment in swift
+    j += 1
+} while j < 10
+
+
+//using while
+var k = 0
+while k < 11 {
+    print(k)
+    k += 1
+}
