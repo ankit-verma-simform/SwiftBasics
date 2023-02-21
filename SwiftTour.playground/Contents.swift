ea577f1537533c7cb0cf3d377cdfa0f706409aa6
ankit-verma-simform <ankit.verma@simformsolutions.com>
2023-02-21 16:56:56 +0530

    variables and swift concepts

--- SwiftTour.playground/Contents.swift
+++ SwiftTour.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var greeting = "Hello, swift"
+
+var mutableVariables: Int = 32
+print(mutableVariables)
+
+mutableVariables = 43 //new value can be assigned
+
+let myConstant = "value"
+//myConstant = "newValue"  //can't reassign a constant, uncommenting will throw error
+
+//a variable of a specific type can't hold value of another type
+//mutableVariables = "stringValue" //uncomment to see error
+
+var uninitializedVariable: Bool //using uninitialized variable throws error
+//print(uninitializedVariable) //uncomment to see error
+
+var concatedString = "I'm a " + "concated string" //use + to concat strings
+//var cantConcat = concatedString + " " + mutableVariables //swift don't allow implicit type conversion
+var cantConcat = concatedString + " " + String(mutableVariables) //explicitly converting Int to String
+
