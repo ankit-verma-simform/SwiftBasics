68293a7d847cc8e555ace203ae29e1e8566ff7b2
ankit-verma-simform <ankit.verma@simformsolutions.com>
2023-02-23 18:43:19 +0530

    practice

--- SwiftTour.playground/Contents.swift
+++ SwiftTour.playground/Contents.swift
@@ -1,3 +1,51 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let nums: [Int] = [4, 6, 8, 10, 5, 2]
+
+// printing num from nums
+print("Nums:")
+for num in nums {
+    print(num)
+}
+
+// summing up ints in array
+var sum = 0
+print("Sum:", terminator: "")
+for (_, num) in nums.enumerated() {
+    sum += num
+}
+print(sum)
+
+// using dictionaries
+let emojis = [
+    "Ghost" : "👻",
+    "Alien monster" : "👾​​",
+    "Scream" : "😱"
+]
+print(emojis["Ghost"] ?? "No such emoji!")
+
+// forced unwrapping
+let a: String? = nil
+if a != nil {
+    print(a!)
+}
+
+// optional binding
+if let a = a {
+    print(a)
+}
+
+let b = String()
+print(b)
+
+// declaring multiple letiables
+let x = 3, y = 4.2, z = "random"
+print(x)
+
+// using keyword as identifier by wrapping it within ``
+let `Int`:Int = 91
+print(`Int`)
+
+let three = 3
+let pointOneFour = 0.14
+let pi = three + pointOneFour
