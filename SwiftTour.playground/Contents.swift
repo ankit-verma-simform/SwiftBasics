d64f8a4cb2fc3a65ea0b37db5fa778f648a84b92
ankit-verma-simform <ankit.verma@simformsolutions.com>
2023-02-24 10:52:22 +0530

    completed docs - the basics

--- SwiftTour.playground/Contents.swift
+++ SwiftTour.playground/Contents.swift
@@ -38,14 +38,61 @@ if let a = a {
 let b = String()
 print(b)
 
-// declaring multiple letiables
+// declaring multiple variables
 let x = 3, y = 4.2, z = "random"
 print(x)
 
 // using keyword as identifier by wrapping it within ``
-let `Int`:Int = 91
-print(`Int`)
+//let `Int`:Int = 91
+//print(Int)
 
+//type conversions
 let three = 3
 let pointOneFour = 0.14
-let pi = three + pointOneFour
+let pi = Double(three) + pointOneFour
+
+//using tuples
+let httpStatus = (code:404, message:"Not Found")
+print(httpStatus.message)
+
+//working with optionals
+var possibleNumber = "123"
+//let possibleNumber: Int? = nil
+let convertedNumber = Int(possibleNumber)
+
+let k: Int? = 6
+if var k {
+    print(k)
+}
+//k += 1
+//print(k)
+
+//error handling in swift
+func canThrowAnError() throws {
+    // this function may or may not throw an error
+}
+
+//do {
+    try canThrowAnError()
+    // no error was thrown
+//} catch {
+//    // an error was thrown
+//}
+
+//using assertions and preconditions
+let age = +3
+if age > 10 {
+    print("You can ride the roller-coaster or the ferris wheel.")
+} else if age >= 0 {
+    print("You can ride the ferris wheel.")
+} else {
+    assertionFailure("A person's age can't be less than zero.")
+}
+
+//precondition(false, "Not met the necessary conditions")
+
+/*
+https://stackoverflow.com/questions/29673027/difference-between-precondition-and-assert-in-swift
+*/
+
+
